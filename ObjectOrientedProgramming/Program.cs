8d54b16907682855b7261e3de13dd484e3a006c7
marek-tokarz <marek.tokarz14@gmail.com>
2025-04-15 17:29:08 +0200

    This program purposefully breaks a rule of SOLID programming, a rule called 'Single Responsibility Rule', a code will be refactored later to adhere to the SRP rule. Program simply reads a text file with names if it exists or creates such file after validating an input from users

---
+++ ObjectOrientedProgramming/Program.cs
@@ -0,0 +1,69 @@
+﻿var names = new Names();
+var path = names.BuildFilePath();
+if(File.Exists(path))
+{
+    Console.WriteLine("Names file already exists. Loading names.");
+    names.ReadFromTextFile();
+}
+else
+{
+    Console.WriteLine("Names file does not exists yet.");
+
+    //let's imagine they are given by the user
+    names.AddName("John");
+    names.AddName("not a valid name");
+    names.AddName("Claire");
+    names.AddName("123 definitely not a valid name");
+
+    Console.WriteLine("Saving names to a file.");
+    names.WriteToTextFile();
+}
+Console.WriteLine(names.Format());
+Console.ReadKey();
+
+class Names
+{
+    private List<string> _names = new List<string>();
+
+    public void AddName(string name)
+    {
+        if(IsValidName(name))
+        {
+            _names.Add(name);
+        }
+    }
+
+    private bool IsValidName(string name)
+    {
+        return name.Length >= 2 &&
+            name.Length < 25 &&
+            char.IsUpper(name[0]) &&
+            name.All(char.IsLetter);
+    }
+
+    public void ReadFromTextFile()
+    {
+        var fileContents = File.ReadAllText(BuildFilePath());
+        var namesFromFile = fileContents.Split(Environment.NewLine).ToList();
+        foreach(var name in namesFromFile)
+        {
+            AddName(name);
+        }
+    }
+
+    public void WriteToTextFile()
+    {
+        File.WriteAllText(BuildFilePath(), Format());
+    }
+
+    public string BuildFilePath()
+    {
+        // for simplicity a file name is a fixed value
+        return "names.txt";
+    }
+
+    public string Format()
+    {
+        return string.Join(Environment.NewLine, _names);
+    }
+}
