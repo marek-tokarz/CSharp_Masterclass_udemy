b9c2994cf82786b2693aa3e7866ca96942ce2b56
marek-tokarz <marek.tokarz14@gmail.com>
2025-04-17 18:23:09 +0200

    Renaming a project 'ObjectOrientedProgramming' to a name: 'NamesValidating' and creating a simple game: 'Roll a die' - a user has to guess what number shows up on a die, user has 3 chances to guess, wrong, non int input or out of range input is not considered as a valid chance that user had lost

---
+++ ObjectOrientedProgramming/RollDiceGame/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace RollDiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RollDie rollDice = new RollDie();
+            
+            int number = rollDice.GenerateRandomNumber();
+            NumberGuessing guessNumber = new NumberGuessing();
+
+            guessNumber.TryGuessing(number);
+
+            Console.ReadKey();
+        }
+    }
+
+    public class RollDie
+    {
+        private int randomInRange = 0;
+        private Random random;
+
+        public RollDie()
+        {
+            // Initialize the Random object
+            random = new Random();
+        }
+
+        public int GenerateRandomNumber()
+        {
+            return randomInRange = random.Next(0, 7);
+        }
+    }
+
+    public class NumberGuessing
+    {
+        private bool _userGuessed = false;
+        private int _numberTyped;
+        private int _numberOfTries = 0;
+
+        public void TryGuessing(int number)
+        {
+            Console.WriteLine("Die rolled. Guess what number it shows in 3 tries.");
+            while (!_userGuessed && _numberOfTries < 3)
+            {
+                Console.WriteLine("Enter a number:");
+                var userInput = Console.ReadLine();
+                if (userInput == "")
+                {
+                    Console.WriteLine("Selected number can not be empty");
+                    continue;
+                }
+                if (int.TryParse(userInput, out _numberTyped))
+                {
+                    if(_numberTyped < 1 || _numberTyped > 6)
+                    {
+                        Console.WriteLine("Type a number from range 1 to 6");
+                        continue;
+                    }
+                    if (number == _numberTyped)
+                    {
+                        Console.WriteLine("You win");
+                        _userGuessed = true;
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wrong number");
+                        _numberOfTries++;
+                    }
+                } 
+            }
+            Console.WriteLine("You lose");
+            Console.WriteLine("The number was: " + number);
+        }
+    }
+}
