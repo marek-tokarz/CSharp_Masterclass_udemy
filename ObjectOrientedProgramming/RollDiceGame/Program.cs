e7c795229ea5b960c79fd88e27672c6ce04efa89
marek-tokarz <marek.tokarz14@gmail.com>
2025-04-18 18:38:08 +0200

    Refactoring a game - guessing a number on a dice

--- ObjectOrientedProgramming/RollDiceGame/Program.cs
+++ ObjectOrientedProgramming/RollDiceGame/Program.cs
@@ -1,5 +1,80 @@
 ﻿using System;
 
+class Program1
+{
+    static void Main(string[] args)
+    {
+        var random = new Random();
+        var dice = new Dice(random);
+        var guessingGame = new GuessingGame(dice);
+
+        bool xxx = guessingGame.Play();
+
+        Console.ReadKey();
+    }
+
+    public class GuessingGame
+    {
+        private readonly Dice _dice;
+        private const int InitialTries = 3;
+
+        public GuessingGame(Dice dice)
+        {
+            _dice = dice;
+        }
+
+        public bool Play()
+        {
+            var diceRollResult = _dice.Roll();
+            Console.WriteLine($"Dice rolled. Guess what number it shows in {InitialTries} tries.");
+
+
+            var triesLeft = InitialTries;
+            while(triesLeft > 0)
+            {
+                var guess = ConsoleReader.ReadInteger("Enter a number:");
+                if(guess == diceRollResult)
+                {
+                    return true;
+                }
+                Console.WriteLine("Wrong number");
+                --triesLeft;
+            }
+            return false;
+        }
+    }
+
+    public static class ConsoleReader
+    {
+        public static int ReadInteger(string message)
+        {
+            int result;
+            do
+            {
+                Console.WriteLine(message);
+            } while (!int.TryParse(Console.ReadLine(), out result));
+            return result;
+        }
+    }
+
+    public class Dice
+    {
+        private readonly Random _random;
+        private const int SidesCount = 6;
+
+        public Dice(Random random)
+        {
+            _random = random;
+        }
+
+        public int Roll()
+        {
+            return _random.Next(1, SidesCount + 1);
+        }
+    }
+}
+
+/* First version
 namespace RollDiceGame
 {
     class Program
@@ -77,3 +152,4 @@ namespace RollDiceGame
         }
     }
 }
+*/
