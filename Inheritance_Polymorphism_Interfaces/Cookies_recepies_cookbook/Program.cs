8ce78f7edf48fe4f0c49efeee85001091a0a455c
marek-tokarz <marek.tokarz14@gmail.com>
2025-04-23 18:22:14 +0200

    Adding a global try-catch for an app.

--- Inheritance_Polymorphism_Interfaces/Cookies_recepies_cookbook/Program.cs
+++ Inheritance_Polymorphism_Interfaces/Cookies_recepies_cookbook/Program.cs
@@ -10,26 +10,38 @@ namespace Program;
 
 public static class Program
 {
+    
     public static void Main(string[] args)
     {
-        const FileFormat Format = FileFormat.Json;
+        try // global try-catch
+        {
+            const FileFormat Format = FileFormat.Json;
 
-        IStringsRepository stringsRepostory = Format == FileFormat.Json ?
-            new StringsJsonRepository() :
-            new StringsTextualRepository();
+            IStringsRepository stringsRepostory = Format == FileFormat.Json ?
+                new StringsJsonRepository() :
+                new StringsTextualRepository();
 
-        const string FileName = "recipes";
-        var fileMetadata = new FileMetadata(FileName, Format);
+            const string FileName = "recipes";
+            var fileMetadata = new FileMetadata(FileName, Format);
 
-        var ingredientsRegister = new IngredientsRegister();
+            var ingredientsRegister = new IngredientsRegister();
 
-        var cookiesRecipesApp = new CookiesRecipesApp(
-            new RecipesRepository(
-                stringsRepostory,
-                ingredientsRegister),
-            new RecipesConsoleUserInteraction(
-                ingredientsRegister));
-        cookiesRecipesApp.Run(fileMetadata.ToPath());
+            var cookiesRecipesApp = new CookiesRecipesApp(
+                new RecipesRepository(
+                    stringsRepostory,
+                    ingredientsRegister),
+                new RecipesConsoleUserInteraction(
+                    ingredientsRegister));
+            cookiesRecipesApp.Run(fileMetadata.ToPath());
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine("Sorry! The application experienced" +
+                " an unexpected error and will have to b closed" +
+                "The error message: " + ex.Message);
+            Console.WriteLine("Press any key to close.");
+            Console.ReadKey();
+        }
     }
 }
 
