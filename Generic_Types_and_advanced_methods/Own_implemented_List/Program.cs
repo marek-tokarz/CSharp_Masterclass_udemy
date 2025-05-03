7003d904b3dbd0b3fe47cc520432491a387dc495
Marek Tokarz <marek.tokarz14@gmail.com>
2025-05-03 11:05:54 +0200

    Self implementing a List, starting with Add(int item) method

---
+++ Generic_Types_and_advanced_methods/Own_implemented_List/Program.cs
@@ -0,0 +1,31 @@
+﻿var numbers = new ListOfInts();
+
+numbers.Add(10);
+numbers.Add(20);
+numbers.Add(30);
+numbers.Add(40);
+numbers.Add(50);
+
+Console.ReadKey();
+
+class ListOfInts
+{
+    private int[] _items = new int[4];
+    private int _size = 0;
+
+    public void Add(int item)
+    {
+        if(_size >= _items.Length)
+        {
+            var newItems = new int[_items.Length * 2];
+
+            for(int i = 0; i < _items.Length; i++)
+            {
+                newItems[i] = _items[i];
+            }
+            _items = newItems;
+        }
+        _items[_size] = item;
+        ++_size;
+    }
+}
