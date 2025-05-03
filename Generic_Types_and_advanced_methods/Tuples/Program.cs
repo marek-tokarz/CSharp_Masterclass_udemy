8cbb7a0255df4d7602309cc2b86b7f60e4aacdb5
marek-tokarz <marek.tokarz14@gmail.com>
2025-05-03 15:53:04 +0200

    A simple class TwoInts that simulates how tuples work - as a showcase of a need for tuples and its behavior

---
+++ Generic_Types_and_advanced_methods/Tuples/Program.cs
@@ -0,0 +1,46 @@
+﻿var numbers = new List<int> { 5, 3, 2, 8, 16, 7 };
+TwoInts minAndMax = GetMinAndMax(numbers);
+Console.WriteLine("Smallest number is" + minAndMax.Int1);
+Console.WriteLine("Largest number is" + minAndMax.Int2);
+
+Console.ReadKey();
+
+TwoInts GetMinAndMax(IEnumerable<int> input)
+{
+    if(!input.Any())
+    {
+        throw new InvalidOperationException(
+            $"The input collection cannot be empty.");
+    }
+
+    int min = input.First();
+    int max = input.First();
+
+    foreach(var number in input)
+    {
+        if(number < min)
+        {
+            min = number;
+        }
+
+        if(number > max)
+        {
+            max = number;
+        }
+    }
+
+    return new TwoInts(min, max);
+}
+
+
+public class TwoInts
+{
+    public TwoInts(int int1, int int2)
+    {
+        Int1 = int1;
+        Int2 = int2;
+    }
+
+    public int Int1 { get; }
+    public int Int2 { get; }
+}
