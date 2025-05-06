8b4ded7466956a94e87f9ab17252c2ed79a5a9eb
marek-tokarz <marek.tokarz14@gmail.com>
2025-05-06 17:32:30 +0200

    Implementing an IComparable<T> interface and sorting an instances of a custom class

---
+++ Generic_Types_and_advanced_methods/IComparable_Interface/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq.Expressions;
+
+var numbers = new List<int> { 5, 1, 7, 2 };
+numbers.Sort();
+
+var words = new List<string> { "ddd", "aaa", "ccc", "bbb" };
+words.Sort();
+
+var people = new List<Person>
+{
+    new Person {Name = "John", YearOfBirth = 1980},
+    new Person {Name = "Anna", YearOfBirth = 1915},
+    new Person {Name = "Bill", YearOfBirth = 2011},
+};
+
+// will throw exception without IComparable<T> interface
+// people.Sort();
+
+people.Sort();
+
+Console.ReadKey();
+
+public class Person : IComparable<Person>
+{
+    public string Name { get; init; }
+    public int YearOfBirth { get; init; }
+
+    public int CompareTo(Person other)
+    {
+        if(this.YearOfBirth < other.YearOfBirth)
+        {
+            return 1;
+        } 
+        else if (this.YearOfBirth > other.YearOfBirth)
+        {
+            return -1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
+
+public class Employee : Person
+{
+    public void GoToWork() => Console.WriteLine("Going to work");
+}
