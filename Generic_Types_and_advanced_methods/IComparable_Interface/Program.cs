079f2f6ef257552383213c93c3aa7c8822e6f406
marek-tokarz <marek.tokarz14@gmail.com>
2025-05-06 18:25:41 +0200

    Implementing a generic method that compares object that implement an IComparable interface

--- Generic_Types_and_advanced_methods/IComparable_Interface/Program.cs
+++ Generic_Types_and_advanced_methods/IComparable_Interface/Program.cs
@@ -16,15 +16,36 @@ var people = new List<Person>
 // will throw exception without IComparable<T> interface
 // people.Sort();
 
+var anna = new Person { Name = "John", YearOfBirth = 1980 };
+var john = new Person { Name = "Anna", YearOfBirth = 1915 };
+
 people.Sort();
 
+PrintInOrder(10, 5);
+PrintInOrder("aaa", "bbb");
+PrintInOrder(anna, john);
+
 Console.ReadKey();
+
+void PrintInOrder<T>(T first, T second) where T: IComparable<T>
+{
+    if(first.CompareTo(second) > 0)
+    {
+        Console.WriteLine($"{second} {first}");
+    }
+    else
+    {
+        Console.WriteLine($"{first} {second}");
+    }
+}
 
 public class Person : IComparable<Person>
 {
     public string Name { get; init; }
     public int YearOfBirth { get; init; }
 
+    public override string ToString() => $"{Name} born in {YearOfBirth}";
+        
     public int CompareTo(Person other)
     {
         if(this.YearOfBirth < other.YearOfBirth)
