26f44398e0756212e579df86b22ab246ee9ba6ce
marek-tokarz <marek.tokarz14@gmail.com>
2025-05-09 15:54:39 +0200

    Funcs and Actions - implemented with simple methods

---
+++ Generic_Types_and_advanced_methods/Funcs_Actions/Program.cs
@@ -0,0 +1,78 @@
+﻿var numbers = new[] { 1, 4, 7, 19, 2 };
+
+Func<int, bool> predicate1 = IsLargerThan10;
+Console.WriteLine(
+    "IsAnyLargerThan10? " + IsAny(numbers, predicate1));
+
+// we can pass directly a methods (we do not need a variables)
+Console.WriteLine(
+    "IsAnyLargerThan10? " + IsAny(numbers, IsLargerThan10));
+
+Func<int, bool> predicate2 = IsEven;
+Console.WriteLine(
+    "IsAnyEven " + IsAny(numbers, predicate2));
+
+// we can pass directly a methods (we do not need a variables)
+Console.WriteLine(
+    "IsAnyEven " + IsAny(numbers, IsEven));
+
+Console.ReadKey();
+
+Func<int, DateTime, string, decimal> someFunc;
+// takes: int, DateTime, string and returns decimal
+
+Action<string, string, bool> someAction;
+// takes: string, string, bool and returns void (a void function)
+
+bool IsLargerThan10(int number)
+{
+    return number > 10;
+}
+
+bool IsEven(int number)
+{
+    return number % 2 == 0;
+}
+
+bool IsAny(
+    IEnumerable<int> numbers,
+    Func<int,bool> predicate)
+{
+    foreach (var number in numbers)
+    {
+        if (predicate(number))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+// not needed after refactor
+/*
+
+bool IsAnyLargerThan10(
+    IEnumerable<int> numbers)
+{
+    foreach(var number in numbers)
+    {
+        if(number > 10)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool IsAnyEven(IEnumerable<int> numbers)
+{
+    foreach (var number in numbers)
+    {
+        if (number % 2 == 0)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+*/
