895e21f8e70560d957daa33430de1e7ecfade392
marek-tokarz <marek.tokarz14@gmail.com>
2025-05-12 18:09:33 +0200

    Refactor - filtering strategies are now kept in an IEnumerable interface and a method FilterBy is a generic method, accepting any type of collection

--- Generic_Types_and_advanced_methods/StrategyDesignPattern/Program.cs
+++ Generic_Types_and_advanced_methods/StrategyDesignPattern/Program.cs
@@ -1,16 +1,22 @@
 ﻿var numbers = new List<int> { 10, 12, -100, 55, 17, 22 };
+var filteringStrategySelector = new FilteringStrategySelector();
 
-Console.WriteLine(@"Select filter:
-Even
-Odd
-Positive");
+Console.WriteLine(@"Select filter:");
+Console.WriteLine(string.Join(
+    Environment.NewLine, 
+    filteringStrategySelector.FilteringStrategiesNames));
 
 var userInput = Console.ReadLine();
 
 var filteringStrategy = new FilteringStrategySelector().Select(userInput);
-List<int> result = new NumbersFilter().FilterBy(filteringStrategy, numbers);
+var result = new Filter().FilterBy(filteringStrategy, numbers);
 
 Print(result);
+
+var words = new[] { "zebra", "ostrich", "otter" };
+var oWords = new Filter().FilterBy(
+    word => word.StartsWith("o"),
+    words);
 
 Console.ReadKey();
 void Print(IEnumerable<int> numbers)
@@ -26,7 +32,11 @@ public class FilteringStrategySelector
             ["Even"] = number => number % 2 == 0,
             ["Odd"] = number => number % 2 == 1,
             ["Positive"] = number => number > 0,
+            ["Negative"] = number => number < 0,
         };
+
+    public IEnumerable<string> FilteringStrategiesNames =>
+        _filteringStrategies.Keys;
 
     public Func<int, bool> Select(string filteringType)
     {
@@ -40,11 +50,13 @@ public class FilteringStrategySelector
     }
 }
 
-public class NumbersFilter
+public class Filter
 { 
-    public List<int> FilterBy(Func<int, bool> predicate, List<int> numbers)
+    public IEnumerable<T> FilterBy<T>(
+        Func<T, bool> predicate,
+        IEnumerable<T> numbers)
     {
-        var result = new List<int>();
+        var result = new List<T>();
 
         foreach (var number in numbers)
         {
