4f90f073c055ee104ee9d5330e5ed833af622e73
marek-tokarz <marek.tokarz14@gmail.com>
2025-05-15 19:13:55 +0200

    Adding a new feature of printing to the Console by a new Decorator

--- Generic_Types_and_advanced_methods/Simple_cache/Program.cs
+++ Generic_Types_and_advanced_methods/Simple_cache/Program.cs
@@ -1,4 +1,7 @@
-﻿IDataDownloader dataDownloader = new CachingDataDownloader(new SlowDataDownloader());
+﻿IDataDownloader dataDownloader = 
+    new CachingDataDownloader(
+        new PrintingDataDownloader(
+            new SlowDataDownloader()));
 
 Console.WriteLine(dataDownloader.DownloadData("id1"));
 Console.WriteLine(dataDownloader.DownloadData("id2"));
@@ -37,6 +40,23 @@ public class CachingDataDownloader : IDataDownloader
     public string DownloadData(string resourceId)
     {
         return _cache.Get(resourceId, _dataDownloader.DownloadData);
+    }
+}
+
+public class PrintingDataDownloader : IDataDownloader
+{
+    private readonly IDataDownloader _dataDownloader;
+ 
+    public PrintingDataDownloader(IDataDownloader dataDownloader)
+    {
+        _dataDownloader = dataDownloader;
+    }
+
+    public string DownloadData(string resourceId)
+    {
+        var data = _dataDownloader.DownloadData(resourceId);
+        Console.WriteLine("Data is ready!");
+        return data;
     }
 }
 
