4803586f1ca6a1147bd366980f0fed4b4e834302
marek-tokarz <marek.tokarz14@gmail.com>
2025-06-02 17:24:19 +0200

    Method to check if a collection has any upper case or lower case only - later this method will be written with LINQ

---
+++ LINQ/LINQBasics/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wordsNoUppercase = new string[]
+            {
+                "quick", "brown", "fox"
+            };
+            Console.WriteLine(IsAnyWordUpperCase(wordsNoUppercase));
+            var wordsWithUppercase = new string[]
+            {
+                "quick", "brown", "FOX"
+            };
+            Console.WriteLine(IsAnyWordUpperCase(wordsWithUppercase));
+            Console.ReadKey();
+        }
+
+        public static bool IsAnyWordUpperCase(
+            IEnumerable<string> words)
+        {
+            foreach(var word in words)
+            {
+                bool areAllUpperCase = true;
+                foreach(var letter in word)
+                {
+                    if(char.IsLower(letter))
+                    {
+                        areAllUpperCase = false;
+                    }
+                }
+                if(areAllUpperCase)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
